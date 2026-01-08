b12d9d58aa06a896e12c2f385ab57ab2d0463939
Avraham Appel <aappel248@gmail.com>
2026-01-08 15:27:34 -0500

    Load wasm successfully

--- package.sh
+++ package.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+rm -rf pkg
+rm bus-extension.xpi
+
 wasm-pack build --target no-modules --no-typescript --no-pack
 
 cp manifest.json pkg/
+cp script.js pkg/
 
 zip -j -r bus-extension.xpi pkg -x '*.gitignore'
---
+++ script.js
@@ -0,0 +1,1 @@
+wasm_bindgen(chrome.runtime.getURL('bus_extension_bg.wasm'))
