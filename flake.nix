8ecb1c7a72ecfb57422884e40b99f4d12f05af5c
Avraham Appel <aappel248@gmail.com>
2026-01-25 12:33:48 -0500

    Refactor to flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = ''A Firefox extension to enhance the "Where's my bus" page of https://tstg.mybusplanner.ca'';
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+
+    flake-parts.lib.mkFlake { inherit inputs; } ({ ... }: {
+
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+
+      perSystem = { config, pkgs, ... }: {
+        packages.bus-extension = pkgs.callPackage ./. { };
+        packages.default = config.packages.bus-extension;
+
+        devShells.default = pkgs.mkShell {
+          packages = config.packages.default.nativeBuildInputs ++ (with pkgs; [
+            bacon
+            clippy
+            rust-analyzer
+            rustfmt
+            zip
+          ]);
+        };
+      };
+    });
+}
