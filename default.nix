7e2f558d77b06e5a00faeb62dafdd77bc4f2496b
Avraham Appel <aappel248@gmail.com>
2026-03-25 21:32:50 -0400

    Unpin wasm-bindgen

--- default.nix
+++ default.nix
@@ -2,6 +2,8 @@
 , lib
 , stdenv
 , rustPlatform
+, fetchCrate
+, buildWasmBindgenCli
 , binaryen
 , cargo
 , rustc
@@ -23,8 +25,29 @@ let
       (throw "wasm-bindgen package not found in Cargo.lock")
       (lib.fromTOML (lib.readFile ./Cargo.lock)).package).version;
 
+  wasmBindgenCliSourceHashes = {
+    "0.2.114" = "sha256-xrCym+rFY6EUQFWyWl6OPA+LtftpUAE5pIaElAIVqW0=";
+  };
+  wasmBindgenCliVendorHashes = {
+    "0.2.114" = "sha256-Z8+dUXPQq7S+Q7DWNr2Y9d8GMuEdSnq00quUR0wDNPM=";
+  };
+
+  wasmBindgenCliBuilt = buildWasmBindgenCli rec {
+    src = fetchCrate {
+      pname = "wasm-bindgen-cli";
+      version = wasmBindgenVersion;
+      hash = wasmBindgenCliSourceHashes.${wasmBindgenVersion} or lib.fakeHash;
+    };
+    cargoDeps = rustPlatform.fetchCargoVendor {
+      inherit src;
+      inherit (src) pname version;
+      hash = wasmBindgenCliVendorHashes.${wasmBindgenVersion} or lib.fakeHash;
+    };
+  };
+
   wasm-bindgen-cli =
-    pkgs.${"wasm-bindgen-cli_${lib.replaceString "." "_" wasmBindgenVersion}"};
+    pkgs.${"wasm-bindgen-cli_${lib.replaceString "." "_" wasmBindgenVersion}"}
+      or wasmBindgenCliBuilt;
 in
 
 stdenv.mkDerivation {
